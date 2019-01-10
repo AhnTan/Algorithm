348cc4e0c2351e02351d5ab3eb67d25d2eb54879
Ahntan <h7678036@naver.com>
2019-01-10 17:58:25 +0900

    update

--- SW_EXPERT_SSAFY_CLUB_6782/src/Solution.java
+++ SW_EXPERT_SSAFY_CLUB_6782/src/Solution.java
@@ -8,7 +8,8 @@ public class Solution {
 	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 	
-	int case_size, init_num, count;
+	int case_size, count;
+	long init_num;
 	
 	public static void main(String[] args) {
 		new Solution();
@@ -21,10 +22,10 @@ public class Solution {
 	public void init() {
 		try {
 			case_size = Integer.parseInt(reader.readLine());
-			System.out.println(Math.pow(Math.sqrt(100), 2.0));
+			
 			for(int i=0; i<case_size; i++) {
 				count = 0;
-				init_num = Integer.parseInt(reader.readLine());
+				init_num = Long.parseLong(reader.readLine());
 				writer.write("#" + (i+1) + " ");
 				calc(init_num);
 			}
@@ -34,23 +35,31 @@ public class Solution {
 		}catch(Exception e) {}
 	}
 	
-	public void calc(double num) {
+	public void calc(long num) {
 		try {
-			
+
 			double temp = Math.sqrt(num);
 			
-			if ((int) num == 2) {
+			if ((long)num == 2) {
 				writer.write(count + "\n");
 				return;
-			} else if (Math.pow(temp, 2.0) == num) {
-				System.out.println("!! : " + num);
-				count++;
-				calc(temp);
+			} else if ( (Math.pow((long)temp, 2.0) != num) || num==1) {
+				
+				// 제곱근을 정수로 변환시켜 1을 더해준다.
+				// 그 더한 값을 제곱한것이 조건에 부합하는 가장 가까운 수(n)이므로 원래 값인(num)을 빼주면 한번에 그 값까지의 카운터가 된다.  
+				Long n = (long)temp;
+				n += 1;
+				n = (long)Math.pow(n, 2.0);
+				
+				count += (n - num);
+				calc(n);
+				
+				
 			} else {
-				System.out.println(num);
 				count++;
-				calc(num + 1.0);
+				calc((long)temp);
 			}
+			
 		}catch(Exception e) {}
 	}
 	
